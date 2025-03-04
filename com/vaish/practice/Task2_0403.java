0d11c41ac1167e9887006a684d60264830abfb64
Vishanthan shree <86905030+vishanthan8055@users.noreply.github.com>
2025-03-04 23:00:19 +0530

    Implemented Reverse Traverse Approach

--- com/vaish/practice/Task2_0403.java
+++ com/vaish/practice/Task2_0403.java
@@ -8,23 +8,29 @@ public class Task2_0403 {
        Scanner sc = new Scanner(System.in);
        System.out.println("Enter Array Size : ");
        int n = sc.nextInt();
-       int t = n;
+       // int t = n;
        int arr[] = new int[n];
        int[] revArr = new int[n];
 //       for(int i = 0 ; i< n ; i++){
 //           arr[i] = sc.nextInt();
 //       }
-       int i =0;
-       while(t !=0){
+      for(int i = n - 1; i>= 0; i--){
+         arr[i] = sc.nextInt();
+      }
+       int i = n-1;
+       while(i != 0){
            arr[i] =sc.nextInt();
-           i++;
-           t--;
+           i--;
        }
        System.out.println(Arrays.toString(arr));
-       revArr[n-1] = arr[0];
-       revArr[n-2] = arr[1];
-       revArr[n-3] = arr[2];
-
+       // revArr[n-1] = arr[0];
+       // revArr[n-2] = arr[1];
+       // revArr[n-3] = arr[2];
+      i = n - 1;
+      while(i != 0){
+         System.out.println(arr[i);
+         i--;
+      }
        System.out.println(Arrays.toString(revArr));
    }
 }
