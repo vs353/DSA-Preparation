d4327c6d240410f9b134869f833b7d112bd66258
vs353 <vaishnavisridhar007@gmail.com>
2025-03-04 22:33:37 +0530

    Problem Solving Task_06

--- com/vaish/practice/Task5_0403.java
+++ com/vaish/practice/Task5_0403.java
@@ -2,6 +2,22 @@ package com.vaish.practice;
 
 public class Task5_0403 {
     public static void oddIndex(){
-
+        int[] arr = {1,2,3,4,5,6};
+        int s =0;
+        for(int i =0 ; i <arr.length; i++){
+            if(i%2!= 0){
+                s = s + arr[i];
+                System.out.println(arr[i]);
+            }
+        }
+        System.out.println(s);
+        int sum = 0;
+        for(int i =arr.length-1 ; i >=0 ; i--){
+            if(i%2!= 0){
+                sum = sum + arr[i];
+                System.out.println(arr[i]);
+            }
+        }
+        System.out.println(sum);
     }
 }
