4ff33d6c614c1bddcab16445e2d7e328948656d6
Vishanthan shree <86905030+vishanthan8055@users.noreply.github.com>
2025-03-04 23:05:05 +0530

    Simplified Arithmetic Operations

--- com/vaish/practice/Task3_0403.java
+++ com/vaish/practice/Task3_0403.java
@@ -21,7 +21,7 @@ public class Task3_0403 {
         int sum =0;
         int l = arr.length -1;
         for(int i = l; i>=0; i--){
-            sum = sum + arr[i];
+            sum += arr[i];
         }
         return sum;
     }
@@ -29,7 +29,7 @@ public class Task3_0403 {
     public static int sum(int[] arr){
         int sum = 0;
         for(int i = 0; i< arr.length; i++){
-            sum = sum+ arr[i];
+            sum += arr[i];
         }
 
         return sum;
