cc60b8c9cace9817b14d7925b3ab76c8da441c1e
vs353 <vaishnavisridhar007@gmail.com>
2025-03-06 13:32:02 +0530

    Main Class

--- com/vaish/main/Main.java
+++ com/vaish/main/Main.java
@@ -1,33 +1,83 @@
 package com.vaish.main;
 
-import com.vaish.concepts.arrays.Arrays;
-import com.vaish.concepts.digits.CountDigits;
+import com.vaish.concepts.recursion.*;
+import com.vaish.practice.*;
 
-import static com.vaish.concepts.digits.ArmstrongNumber.armstrongNumber;
-import static com.vaish.concepts.digits.CheckForPrime.isPrime;
-import static com.vaish.concepts.digits.GCD_HCF.isGCD_HCF;
-import static com.vaish.concepts.digits.PalindromeNumber.palindromeNumbers;
-import static com.vaish.concepts.digits.PrintAllDivisions.printDivisions;
-import static com.vaish.concepts.digits.ReverseNumber.reverseNumbers;
+import java.util.Arrays;
+
+import static com.vaish.concepts.recursion.Recursion.f;
+import static com.vaish.practice.IisHappy.isHappy;
+import static com.vaish.practice.IsPrime.isPrimeNumber;
+import static com.vaish.practice.MySqrt.mySqrt;
+//import static com.vaish.practice.MySqrt.mySqrt;
 
 public class Main {
     public static void main(String[] args) {
-     System.out.println("HELLO World");
-     Arrays.sample();
-     int countDigits = CountDigits.countDigits(797);
-     System.out.println(countDigits);
-     int reverseNumbers = reverseNumbers(10400);
-     System.out.println(reverseNumbers);
-     boolean palindromNumber = palindromeNumbers(122);
-     System.out.println(palindromNumber);
-     boolean isArmstrongNumber = armstrongNumber(371);
-     System.out.println(isArmstrongNumber);
+//     System.out.println("HELLO World");
+//     Arrays.sample();
+//     int countDigits = CountDigits.countDigits(797);
+//     System.out.println(countDigits);
+//     int reverseNumbers = reverseNumbers(10400);
+//     System.out.println(reverseNumbers);
+//     boolean palindromNumber = palindromeNumbers(122);
+//     System.out.println(palindromNumber);
+//     boolean isArmstrongNumber = armstrongNumber(371);
+//     System.out.println(isArmstrongNumber);
+//
+//      int prime =
+//              printDivisions(10);
+//        System.out.println(prime);
+//        int isPrime = isPrime(10);
+//        System.out.println(isPrime);
+//        int isGCD_HCF = isGCD_HCF(20,40);
+//        System.out.println(isGCD_HCF);
 
-        printDivisions(36);
-        boolean isPrime = isPrime(7);
-        System.out.println(isPrime);
-        int isGCD_HCF = isGCD_HCF(20,40);
-        System.out.println(isGCD_HCF);
+//        boolean isPrimeNumber = isPrimeNumber(7);
+//        System.out.println(isPrimeNumber);
+
+//        int mySqrt = mySqrt(2147483647);
+//        System.out.println(mySqrt);
+
+//          boolean isHappy = isHappy(19);
+//          System.out.println(isHappy);
+
+//             Pattern.isPattern(3);
+//        System.out.println(mySqrt(345366));
+//        int[] digits = {6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,0,0,0};
+//        System.out.println(Arrays.toString(PlusOne.plusOne(digits)));
+
+//        int[] digits = {1,2,3};
+//        System.out.println(Arrays.toString(PlusOne1.plusOne1(digits)));
+
+/* Tasks */
+//        Task1_0403.gettingArrayFromUser();
+//        Task2_0403.reverseArray();
+//Task3_0403.arraySum();
+//       Task4_0403.evenIndex();
+//Task5_0403.oddIndex();
+//        Task6_0403.evenNumInArray();
+//        Task6_0403.oddNumInArray();
+        /* Recursion */
+//        f();
+        int i = 1;
+        int n = 3;
+        int sum =0;
+//        RecPrintName.printNames(i,n);
+//        RecLinearly.print1ToN(i,n);
+//        RecLinearly.reverseNTo1(n,n);
+//        RecLinearlyByBacktracking.linearlyByBacktracking(n,n);
+//        RecLinearlyByBacktracking.reverseLinearlyByBT(i,n);
+//        RecParameter.recParameter(n,sum);
+//        System.out.println(RecParameter.recFunctional(n));
+//        System.out.println(RecParameter.recFactorial(n));
+        int[] arr = {1,2,3,4,2};
+        int l = 0;
+        int r = arr.length-1;
+//        int [] ans = RecArray.swapArray( arr, l, r);
+//        System.out.println(Arrays.toString(ans));
+
+        int [] fin = RecArray.singleParameter( arr, l);
+        System.out.println(Arrays.toString(fin));
 
     }
 }
