c8060d85d7a2c2282e891a632f3220a6c7f21e44
vs353 <vaishnavisridhar007@gmail.com>
2025-04-03 16:29:32 +0530

    Array Find Union

---
+++ com/strivers/arrays/easy/GFG_FindUnion.java
@@ -0,0 +1,36 @@
+package com.strivers.arrays.easy;
+
+import java.util.ArrayList;
+
+public class GFG_FindUnion {
+    public static ArrayList<Integer> findUnion(int[] arr1, int[] arr2, int n, int m) {
+        int i = 0, j = 0; // pointers
+        ArrayList<Integer > Union=new ArrayList<>(); // Uninon vector
+        while (i < n && j < m) {
+            if (arr1[i] <= arr2[j]) // Case 1 and 2
+            {
+                if (Union.size() == 0 || Union.get(Union.size()-1) != arr1[i])
+                    Union.add(arr1[i]);
+                i++;
+            } else // case 3
+            {
+                if (Union.size() == 0 || Union.get(Union.size()-1) != arr2[j])
+                    Union.add(arr2[j]);
+                j++;
+            }
+        }
+        while (i < n) // IF any element left in arr1
+        {
+            if (Union.get(Union.size()-1) != arr1[i])
+                Union.add(arr1[i]);
+            i++;
+        }
+        while (j < m) // If any elements left in arr2
+        {
+            if (Union.get(Union.size()-1) != arr2[j])
+                Union.add(arr2[j]);
+            j++;
+        }
+        return Union;
+    }
+}
--- com/strivers/main/Main.java
+++ com/strivers/main/Main.java
@@ -1,8 +1,12 @@
 package com.strivers.main;
 
+import com.strivers.arrays.easy.GFG_FindUnion;
 import com.strivers.arrays.easy.LC_CheckArrayIsSorted_1752;
 import com.strivers.arrays.easy.LeftRotate;
 import com.strivers.arrays.easy.ZerosLast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,6 +18,14 @@ public class Main {
 //        LeftRotate.right_Rotate(arr);
 //        LeftRotate.left_Rotate_DPlace(arr,d);
 //        LeftRotate.rotate(arr,d);
-        ZerosLast.moveAllZerosToLast(arr);
+//        ZerosLast.moveAllZerosToLast(arr);
+//        ZerosLast.optimalSolutinZerosToLast(arr);
+        int n = 10, m = 7;
+        int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int arr2[] = {2, 3, 4, 4, 5, 11, 12};
+        ArrayList<Integer> Union  = GFG_FindUnion.findUnion(arr1, arr2, n, m);
+        System.out.println("Union of arr1 and arr2 is ");
+        for (int val: Union)
+            System.out.print(val+" ");
     }
 }
