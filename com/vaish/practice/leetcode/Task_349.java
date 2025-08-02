694641a1ec864b621c70d29bbe00bab44d7a6461
vs353 <vaishnavisridhar007@gmail.com>
2025-08-02 21:56:38 +0530

    Leetcode

--- com/vaish/practice/leetcode/Task_349.java
+++ com/vaish/practice/leetcode/Task_349.java
@@ -26,36 +26,21 @@ public class Task_349 {
     }
 
     public static  int[] intersect(int[] nums1, int[] nums2) {
-        Set<Integer> set = new HashSet<>();
-        Set<Integer>  result = new HashSet<>();
-        if(nums1.length> nums2.length) {
-            for (int num : nums1) {
-                set.add(num);
-            }
-            for (int num : nums2) {
-                if (set.contains(num)) {
-                    result.add(num);
-                }
+        Map<Integer,Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        for(int num : nums1){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        for (int num : nums2) {
+            if (map.containsKey(num) && map.get(num) > 0) {
+                list.add(num);
+                map.put(num, map.get(num) - 1);
             }
         }
-        else {
-            for (int num : nums2) {
-                set.add(num);
-            }
-            for (int num : nums1) {
-                if (set.contains(num)) {
-                    result.add(num);
-                }
-            }
+        int [] result = new int[list.size()];
+        for(int i = 0; i< list.size() ;i++){
+            result[i] = list.get(i);
         }
-
-        int[] arr = new int[result.size()];
-        int i=0;
-        for(int num : result){
-            arr[i++] = num;
-        }
-        return arr;
+        return result;
     }
-
-
 }
