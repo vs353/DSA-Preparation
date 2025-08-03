ebcebf8975d8c4bb5cf318db6e2137b51b30039e
vs353 <vaishnavisridhar007@gmail.com>
2025-08-03 23:12:21 +0530

    Leetcode

--- com/vaish/practice/leetcode/Task_724.java
+++ com/vaish/practice/leetcode/Task_724.java
@@ -1,21 +1,25 @@
 package com.vaish.practice.leetcode;
 
 public class Task_724 {
-    public static int pivotIndex(int[] nums) {
+    public static int pivotIndex(int[] num1) {
         int totalSum = 0;
-        for (int num : nums) {
+        for (int num : num1) {
             totalSum += num;
         }
 
         int leftSum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int rightSum = totalSum - leftSum - nums[i];
+        for (int i = 0; i < num1.length; i++) {
+            int rightSum = totalSum - leftSum - num1[i];
             if (leftSum == rightSum) {
                 return i;
             }
-            leftSum += nums[i];
+            leftSum += num1[i];
         }
 
         return -1;
+    }
+
+    public static int primeNumbers(){
+        return 0;
     }
 }
