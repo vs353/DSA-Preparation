8044d596ac094add85cdfb8c2808217c95aeddf3
vs353 <vaishnavisridhar007@gmail.com>
2025-08-25 20:44:42 +0530

    Leetcode

--- com/strivers/arrays/easy/ZerosLast.java
+++ com/strivers/arrays/easy/ZerosLast.java
@@ -3,6 +3,36 @@ package com.strivers.arrays.easy;
 import java.util.*;
 
 public class ZerosLast {
+    public static int[] findDiagonalOrder(int[][] mat) {
+        if (mat == null || mat.length == 0) return new int[0];
+        int m = mat.length, n = mat[0].length;
+        int[] result = new int[m * n];
+        int r = 0, c = 0;
+        for (int i = 0; i < result.length; i++) {
+            result[i] = mat[r][c];
+            if ((r + c) % 2 == 0) {
+                if (c == n - 1) {
+                    r++;
+                } else if (r == 0) {
+                    c++;
+                } else {
+                    r--;
+                    c++;
+                }
+            }
+            else {
+                if (r == m - 1) {
+                    c++;
+                } else if (c == 0) {
+                    r++;
+                } else {
+                    r++;
+                    c--;
+                }
+            }
+        }
+        return result;
+    }
     public static int longestSubarray(int[] nums) {
         int left = 0, zeros = 0, maxLen = 0;
         for (int right = 0; right < nums.length; right++) {
