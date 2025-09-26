65d43d0566be1bf3ccf3bb869d30784e379b96d6
vs353 <vaishnavisridhar007@gmail.com>
2025-09-26 21:03:31 +0530

    Leetcode

--- com/strivers/arrays/easy/ZerosLast.java
+++ com/strivers/arrays/easy/ZerosLast.java
@@ -240,6 +240,23 @@ class TaskManager {
 }
 
 public class ZerosLast {
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int count = 0;
+        for (int k = n - 1; k >= 2; k--) {
+            int i = 0, j = k - 1;
+            while (i < j) {
+                if (nums[i] + nums[j] > nums[k]) {
+                    count += j - i;
+                    j--;
+                } else {
+                    i++;
+                }
+            }
+        }
+        return count;
+    }
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
         int[] dp = new int[n + 1];
--- com/vaish/main/Main.java
+++ com/vaish/main/Main.java
@@ -59,6 +59,7 @@ public class Main {
     //1,230,118  [23-09-2025]
     //1,221,332  [24-09-2025]
     //1,212,803  [25-09-2025]
+    //1,204,174  [26-09-2025]
     public static void main(String[] args) {
 //     System.out.println("HELLO World");
 //     Arrays.sample();
