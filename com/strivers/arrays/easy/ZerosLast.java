f4d5fa3ad56a21d5a219cd97607775a0abd6cbc7
vs353 <vaishnavisridhar007@gmail.com>
2025-10-03 21:26:55 +0530

    Leetcode

--- com/strivers/arrays/easy/ZerosLast.java
+++ com/strivers/arrays/easy/ZerosLast.java
@@ -240,6 +240,54 @@ class TaskManager {
 }
 
 public class ZerosLast {
+    private static class Cell {
+        int row, col, height;
+        Cell(int r, int c, int h) {
+            row = r;
+            col = c;
+            height = h;
+        }
+    }
+    public int trapRainWater(int[][] heightMap) {
+        int m = heightMap.length, n = heightMap[0].length;
+        if (m <= 2 || n <= 2) return 0;
+        boolean[][] visited = new boolean[m][n];
+        PriorityQueue<Cell> pq = new PriorityQueue<>(new Comparator<Cell>() {
+            public int compare(Cell a, Cell b) {
+                return a.height - b.height;
+            }
+        });
+        for (int i = 0; i < m; i++) {
+            pq.offer(new Cell(i, 0, heightMap[i][0]));
+            pq.offer(new Cell(i, n - 1, heightMap[i][n - 1]));
+            visited[i][0] = true;
+            visited[i][n - 1] = true;
+        }
+        for (int j = 0; j < n; j++) {
+            pq.offer(new Cell(0, j, heightMap[0][j]));
+            pq.offer(new Cell(m - 1, j, heightMap[m - 1][j]));
+            visited[0][j] = true;
+            visited[m - 1][j] = true;
+        }
+        int water = 0;
+        int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+        while (!pq.isEmpty()) {
+            Cell cell = pq.poll();
+            for (int[] d : directions) {
+                int nr = cell.row + d[0];
+                int nc = cell.col + d[1];
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n && !visited[nr][nc]) {
+                    visited[nr][nc] = true;
+                    int neighborHeight = heightMap[nr][nc];
+                    if (cell.height > neighborHeight) {
+                        water += cell.height - neighborHeight;
+                    }
+                    pq.offer(new Cell(nr, nc, Math.max(neighborHeight, cell.height)));
+                }
+            }
+        }
+        return water;
+    }
     public int maxBottlesDrunk(int numBottles, int numExchange) {
         int drank = numBottles;
         int empty = numBottles;
--- com/vaish/main/Main.java
+++ com/vaish/main/Main.java
@@ -66,6 +66,7 @@ public class Main {
     //1,170,167  [30-09-2025]
     //1,162,272  [01-10-2025]
     //1,155,187  [02-10-2025]
+    //1,147,445  [03-10-2025]
     public static void main(String[] args) {
 //     System.out.println("HELLO World");
 //     Arrays.sample();
