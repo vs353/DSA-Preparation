8dbc16d88dabfdbcea05e00d7c88a45a94176006
vs353 <vaishnavisridhar007@gmail.com>
2025-10-27 22:44:43 +0530

    Leetcode

--- com/strivers/arrays/easy/ZerosLast.java
+++ com/strivers/arrays/easy/ZerosLast.java
@@ -238,7 +238,22 @@ class TaskManager {
         return -1;
     }
 }
+
 class Bank {
+    public int numberOfBeams(String[] bank) {
+        int prev = 0, total = 0;
+        for (String row : bank) {
+            int devices = 0;
+            for (char c : row.toCharArray())
+                if (c == '1')
+                    devices++;
+            if (devices > 0) {
+                total += prev * devices;
+                prev = devices;
+            }
+        }
+        return total;
+    }
     long[] balance;
 
     public Bank(long[] balance) {
