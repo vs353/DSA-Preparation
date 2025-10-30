e06abdd964f48182919351afc76aa119066e3502
vs353 <vaishnavisridhar007@gmail.com>
2025-10-30 13:46:55 +0530

    Leetcode

--- com/strivers/arrays/easy/ZerosLast.java
+++ com/strivers/arrays/easy/ZerosLast.java
@@ -240,6 +240,14 @@ class TaskManager {
 }
 
 class Bank {
+    public int minNumberOperations(int[] target) {
+        int res = target[0];
+        for (int i = 1; i < target.length; i++) {
+            if (target[i] > target[i - 1])
+                res += target[i] - target[i - 1];
+        }
+        return res;
+    }
     public int smallestNumber(int n) {
         int x = 1;
         while ((1 << x) - 1 < n)
