81d111eea9c63b656170d9f1d9961a8da2c9b2e2
vs353 <vaishnavisridhar007@gmail.com>
2025-11-16 21:45:39 +0530

    Leetcode

--- com/strivers/arrays/easy/ZerosLast.java
+++ com/strivers/arrays/easy/ZerosLast.java
@@ -189,6 +189,18 @@ class Spreadsheet {
     }
 }
 class TaskManager {
+    public int numSub(String s) {
+        long ans = 0, count = 0, mod = 1000000007;
+        for (char c : s.toCharArray()) {
+            if (c == '1') count++;
+            else {
+                ans = (ans + count * (count + 1) / 2) % mod;
+                count = 0;
+            }
+        }
+        ans = (ans + count * (count + 1) / 2) % mod;
+        return (int) ans;
+    }
     public int[][] rangeAddQueries(int n, int[][] queries) {
         int[][] diff = new int[n + 1][n + 1];
         for (int[] q : queries) {
--- com/vaish/main/Main.java
+++ com/vaish/main/Main.java
@@ -109,6 +109,7 @@ public class Main {
     //926,704    [13-11-2025]
     //927,291    [14-11-2025]
     //921,362    [15-11-2025]
+    //921,983    [16-11-2025]
     public static void main(String[] args) {
 //     System.out.println("HELLO World");
 //     Arrays.sample();
